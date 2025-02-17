6c2edb4451b57a466d379182a185e3f6ebb011e8
LAB53 <lab53@fatec.com>
2025-02-17 10:54:22 -0300

    primeiro commit na fatec

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string name;
+Console.WriteLine("Meu primeiro programa");
+Console.WriteLine("Qual seu nome?");
+
+name  = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine($"Bem vinda {name}");
+
+Console.ReadKey();
