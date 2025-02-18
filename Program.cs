5b5b27853ff1ad996dc2b44ed9e205803680a0ed
Gabriel <ggabrielcunha06@gmail.com>
2025-02-17 21:58:12 -0300

    sistema simples de simulação de caixa

--- Program.cs
+++ Program.cs
@@ -1,9 +1,85 @@
-﻿string name;
-Console.WriteLine("Meu primeiro programa");
-Console.WriteLine("Qual seu nome?");
+﻿Console.WriteLine("Início do programa...");
+Console.WriteLine("Seja bem-vindo(a) ao AntaresShoope");
 
-name  = Convert.ToString(Console.ReadLine());
+Console.Write("Digite um número: ");
+double Desc = double.Parse(Console.ReadLine());
+Console.WriteLine($"Você recebeu {Desc}% de desconto!");
 
-Console.WriteLine($"Bem vinda {name}");
+Desc = Desc / 100;
 
-Console.ReadKey();
+Console.Write("Digite o valor do produto que você deseja: ");
+double prod = double.Parse(Console.ReadLine());
+
+prod *= (1 - Desc); 
+
+Console.WriteLine($"O produto que você deseja está saindo por R${prod:F2}!");
+Console.Write("Você deseja continuar sua compra? ");
+string esc = Console.ReadLine().ToUpper();
+
+if (esc != "SIM" && esc != "S")
+{
+    Console.WriteLine("Tudo bem, tenha um bom dia!");
+}
+else
+{
+    Console.WriteLine("Muito bem!!");
+    Console.Write("Selecione a forma de pagamento (D) - Débito, (C) - Crédito, (P) - Pix: ");
+    string formapagamento = Console.ReadLine().ToUpper();
+
+    if (formapagamento == "D" || formapagamento == "C")
+    {
+        Console.WriteLine($"{(formapagamento == "D" ? "Débito" : "Crédito")} selecionado");
+
+        int senhaCorreta = 2018;
+        int tentativasRestantes = 3;
+
+        while (tentativasRestantes > 0)
+        {
+            Console.Write("Digite sua senha: ");
+            int senhaDigitada = int.Parse(Console.ReadLine());
+
+            if (senhaDigitada == senhaCorreta)
+            {
+                Console.WriteLine("Senha correta!"); 
+                Console.WriteLine("Pagamento aprovado");
+                break;
+            }
+            else
+            {
+                tentativasRestantes--;
+                Console.WriteLine($"Senha incorreta! Tentativas restantes: {tentativasRestantes}");
+
+                if (tentativasRestantes == 0)
+                {
+                    Console.WriteLine("Cartão bloqueado!");
+                    Console.WriteLine("Entre em contato com o banco");
+                    Environment.Exit(1);
+                }
+            }
+        }
+    }
+    else if (formapagamento == "P")
+    {
+        Console.WriteLine("Pix selecionado");
+        Console.Write("Digite a chave Pix: ");
+        string chavePix = Console.ReadLine();
+
+        if (chavePix.Length < 6)
+        {
+            string mensagemErro = $"Erro: Chave Pix inválida - {chavePix} ({DateTime.Now})";
+            Console.WriteLine(mensagemErro);
+            File.AppendAllText("log.txt", mensagemErro + Environment.NewLine);
+        }
+        else
+        {
+            Console.WriteLine("Pagamento via Pix realizado com sucesso!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida!");
+        Environment.Exit(1);
+    }
+
+    Environment.Exit(0);
+}
